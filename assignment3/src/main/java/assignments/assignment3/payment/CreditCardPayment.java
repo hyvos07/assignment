13b3bc84f9c257776e883a14bf95cd4c60f2fba8
Hyvos <daniel.liman07@gmail.com>
2024-05-01 00:26:14 +0700

    add: Payment System

--- assignment3/src/main/java/assignments/assignment3/payment/CreditCardPayment.java
+++ assignment3/src/main/java/assignments/assignment3/payment/CreditCardPayment.java
@@ -1,6 +1,15 @@
 package assignments.assignment3.payment;
 
-public class CreditCardPayment {
-    //TODO: implementasikan class yang implement interface di sini
-    // Anda dibebaskan untuk membuat method yang diperlukan
+public class CreditCardPayment implements DepeFoodPaymentSystem{
+    public static double TRANSACTION_FEE_PERCENTAGE = 0.02;
+
+    // Menghitung biaya transaksi dari kartu kredit
+    public long countTransactionFee(long amount){
+        return (long) (amount * TRANSACTION_FEE_PERCENTAGE);
+    }
+
+    @Override
+    public long processPayment(long amount) {
+        return amount + countTransactionFee(amount);
+    }
 }
--- assignment3/src/main/java/assignments/assignment3/payment/DebitPayment.java
+++ assignment3/src/main/java/assignments/assignment3/payment/DebitPayment.java
@@ -1,6 +1,14 @@
 package assignments.assignment3.payment;
 
-public class DebitPayment {
-    //TODO implementasikan class di sini
-    // Anda dibebaskan untuk membuat method yang diperlukan
+public class DebitPayment implements DepeFoodPaymentSystem{
+    public static double MINIMUM_TOTAL_PRICE = 50000; // Rp 50.000
+    
+    @Override
+    public long processPayment(long amount) {
+        if(amount < MINIMUM_TOTAL_PRICE){
+            return -1;
+        }
+        
+        return amount;
+    }
 }
